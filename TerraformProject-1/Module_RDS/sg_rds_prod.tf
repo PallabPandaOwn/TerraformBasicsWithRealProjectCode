5542e164836cebfd96c0d60cb8cf1162723b282a
Pallab Panda <Pallab.Panda@gmail.com>
2020-10-03 20:27:11 +0530

    added RDS module

---
+++ TerraformProject-1/Module_RDS/sg_rds_prod.tf
@@ -0,0 +1,21 @@
+resource "aws_security_group" "rds-prod"{
+  tags={
+    Name = "${var.PROJECT_NAME}-rds-production"
+  }
+  name = "${var.PROJECT_NAME}-rds-production"
+  vpc_id      = "vpc-705e1217" # please replace subnet ids as per your infra
+  ingress{
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    cidr_blocks = ["${var.RDS_CIDR}"]
+
+  }
+
+  egress{
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
